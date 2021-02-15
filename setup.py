3be715d876acbeb7b2080ff09ef2fb7e8b7b8548
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-02-15 17:44:16 +0200

    Replace ý with y

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/jendas1/rooted-tree-classifier",
-    author="Jan Studený",
+    author="Jan Studeny",
     author_email="jendas1@yahoo.com",
     license="Unlicense",
     classifiers=[
